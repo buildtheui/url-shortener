e01b3298a9cdfe8db698d15f3e1cb58e07d61c7d
Fabian Monsalve <fabtics@gmail.com>
2022-01-11 22:03:19 -0500

    fix: url id naming to redirect

--- src/application/controllers/get-by-short-url.controller.ts
+++ src/application/controllers/get-by-short-url.controller.ts
@@ -1,17 +1,13 @@
-import { Request } from "express"
-import { Controller } from "@application/base/controller"
-import { ShortLinkMongo } from "src/infrastructure/short-link-mongo"
-import { ConfigHelpers } from "@infra/config-helpers"
-import { GetByShortUrl } from "@domain/use-cases/get-by-short-url"
+import { Request } from "express";
+import { Controller } from "@application/base/controller";
+import { ShortLinkMongo } from "src/infrastructure/short-link-mongo";
+import { GetByShortUrl } from "@domain/use-cases/get-by-short-url";
 
 export class GetByShortUrlController extends Controller {
   async forward(req: Request): Promise<object> {
-    const { shortUrlId } = req.params
+    const { shortUrlId } = req.params;
 
-    const newShortLink = new GetByShortUrl(
-      shortUrlId,
-      new ShortLinkMongo()
-    )
-    return await newShortLink.handle()
+    const newShortLink = new GetByShortUrl(shortUrlId, new ShortLinkMongo());
+    return await newShortLink.handle();
   }
 }
--- src/main/routes/get-by-short-url.route.ts
+++ src/main/routes/get-by-short-url.route.ts
@@ -1,19 +1,17 @@
 import { Router } from "express";
 import { adaptRoute } from "@application/adapters/route-adapter";
-import { CreateController } from "@application/controllers/create.controller";
-import { CreatePresenter } from "@application/presenters/create.presenter";
 import { validateRequest } from "@main/middlewares/validate-request";
-import { body, param } from "express-validator";
+import { param } from "express-validator";
+import { GetByShortUrlController } from "@application/controllers/get-by-short-url.controller";
+import { GetByShortUrlPresenter } from "@application/presenters/get-by-short-url.presenter";
 
 const router = Router();
 
 router.get(
-  "/:shortId",
-  [
-    param("shortId").isString().withMessage("Invalid short URL")      
-  ],
+  "/:shortUrlId",
+  [param("shortUrlId").isString().withMessage("Invalid short URL")],
   validateRequest,
-  adaptRoute(new CreateController(), new CreatePresenter())
+  adaptRoute(new GetByShortUrlController(), new GetByShortUrlPresenter())
 );
 
 export { router as GetByIdRouter };
